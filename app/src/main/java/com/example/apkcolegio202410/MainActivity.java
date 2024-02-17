d356d465b832b9c072fdb4a8b1a77bd8b98ed868
joseubillus <jose_ubillus@hotmail.com>
2024-02-16 20:44:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/apkcolegio202410/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.apkcolegio202410;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OnClick_BtnAcep(View v){
+        startActivity(new Intent(MainActivity.this,MnLogin.class));
+    }
+}
